4b4320c60f812ac834e7564b56a496e53067a44e
Murad014 <quliyev.murad@yahoo.com>
2022-12-23 10:15:07 +0400

    Upload All files except node_modules

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: "./app/index.jsx",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "index_bundle.js",
+        publicPath: '/'
+    },
+
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/, use: "babel-loader"},
+            {test: /\.css$/, use: ["style-loader", "css-loader"]}
+
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: [".jsx", "..."]
+    },
+
+    mode: "development",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "app/index.html",
+            
+        })
+    ]
+}
